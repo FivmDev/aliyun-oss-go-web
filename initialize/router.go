bc21000ee3d511fe65c55819c0429a9afc982d73
FivmDev <2296729555@qq.com>
2025-09-27 19:28:14 +0800

    更新初始化函数内容

--- initialize/router.go
+++ initialize/router.go
@@ -1,5 +1,13 @@
 package initialize
 
-func InitRouter() {
-	
+import (
+	"aliyun-oss-go-web/router"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	g := gin.Default()
+	v1 := g.Group("/v1")
+	router.InitV1Router(v1)
+	return g
 }
