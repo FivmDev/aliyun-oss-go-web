a7ea5ac36319a6c701e219798673c7f6748082de
FivmDev <2296729555@qq.com>
2025-09-27 20:58:01 +0800

    优化初始化路由函数

--- initialize/router.go
+++ initialize/router.go
@@ -9,7 +9,7 @@ func InitRouter() *gin.Engine {
 	g := gin.Default()
 	// 确保 templates 目录存在，且 index.html 在该目录下
 	g.LoadHTMLGlob("templates/*")
-	g.Static("/static", "../static")
+	g.Static("/static", "./static")
 	v1 := g.Group("v1")
 	router.InitV1(v1)
 	return g
