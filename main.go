1fa76e6591662ddb83c49b5b430b80e5f8267553
FivmDev <2296729555@qq.com>
2025-09-27 20:53:19 +0800

    优化main主函数

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"aliyun-oss-go-web/initialize"
 	"fmt"
-	"net/http"
 	"os"
 )
 
@@ -19,14 +19,22 @@ func main() {
 	}
 	// 打印服务器运行的地址和端口
 	fmt.Printf("server is running on %s \n", strIPPort)
-	// 注册处理根路径请求的函数
-	http.HandleFunc("/", handlerRequest)
-	// 注册处理获取签名请求的函数
-	http.HandleFunc("/get_post_signature_for_oss_upload", handleGetPostSignature)
-	// 启动HTTP服务器
-	err := http.ListenAndServe(strIPPort, nil)
+	//初始化路由
+	router := initialize.InitRouter()
+	//启动路由
+	err := router.Run(strIPPort)
 	if err != nil {
-		strError := fmt.Sprintf("http.ListenAndServe failed : %s \n", err.Error())
+		strError := fmt.Sprintf("router run failed : %s \n", err.Error())
 		panic(strError)
 	}
+	//// 注册处理根路径请求的函数
+	//http.HandleFunc("/", handlerRequest)
+	//// 注册处理获取签名请求的函数
+	//http.HandleFunc("/get_post_signature_for_oss_upload", handleGetPostSignature)
+	//// 启动HTTP服务器
+	//err := http.ListenAndServe(strIPPort, nil)
+	//if err != nil {
+	//	strError := fmt.Sprintf("http.ListenAndServe failed : %s \n", err.Error())
+	//	panic(strError)
+	//}
 }
